1fcec8ce9c76dcc7d363fc82207f521133bfdeec
Michael Jackson-Potter <nzmikejp@gmail.com>
2022-12-21 11:50:32 +1300

    updating stylelint.yaml

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,7 +1,7 @@
 module.exports = {
-  siteName: "11ty Starter Template",
-  siteDescription: "Quickly launch an 11ty-generated static site. Includes a minimal Sass framework, and generated sitemap, RSS feed, and social share preview images.",
-  siteUrl: "https://11ty-starter-template.netlify.com",
+  siteName: "11ty Website Starter Template",
+  siteDescription: "Quickly launch an 11ty-generated static site. Includes a minimal Sass framework, and generated sitemap, RSS feed",
+  siteUrl: "",
   siteImage: "",
   authorName: "Mike J Potter",
   favicon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👍🏻</text></svg>"
